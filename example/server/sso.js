4480fd7594f4c1d70cfa0622179450e875d1dad6
emgee3 <hello@gravitronic.com>
2015-02-16 17:35:58 -0800

    update example

--- example/server/sso.js
+++ example/server/sso.js
@@ -1,10 +1,11 @@
-SSO.debug        = true;
+SSO.debug        = false;
 SSO.createUsers  = true;
-SSO.getUserProps = function (opts) {
-  opts = opts || {};
-  console.log('called');
-  // var userdata = HTTP.get("url", { params : { domain : opts.domain, user : opts.username } });
-  // if (userdata.data.admin) return { admin : true };
-  return { admin : true, profile : { name : opts.username.toUpperCase() } };
-}
+SSO.getUserProps = Meteor.wrapAsync(getUserProps)
 SSO.serverToken = "5c292f53-3e7e-4d65-89f6-6f316d9b26df";
+
+function getUserProps (opts, cb) {
+  opts = opts || {};
+  var userdata = HTTP.get("url", { params : { domain : opts.domain, user : opts.username } });
+  if (userdata.data.admin) return cb(null, { admin : true });
+  return cb(null, {});
+}
