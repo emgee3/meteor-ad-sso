cd3fa32162f500b96f249fc1e52f5abb09d4f0a4
emgee3 <hello@gravitronic.com>
2015-02-16 20:06:57 -0800

    fix package.js and publish

--- package-meteor-ad-sso/package.js
+++ package-meteor-ad-sso/package.js
@@ -1,24 +1,32 @@
 Package.describe({
   name : "emgee:ad-sso",
   summary: "Active Directory SSO",
-  version: "0.1.0",
-  git: "https://github.com/emgee3/meteor-ad-sso.git"
+  version: "0.1.1",
+  git: "https://github.com/emgee3/meteor-ad-sso.git",
+  documentation: "README.md"
 });
 
 Npm.depends({ "chalk" : "0.5.1" });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
   api.versionsFrom("METEOR@1.0.3.1");
 
-  api.use("iron:router@1.0.7");
+  api.use("templating", "client");
+  api.use("jquery", "client");
+  api.use("session", "client");
+  api.use("tracker", "client");
+  api.use("reactive-var", "client");
 
-  api.use(["templating", "jquery", "session", "tracker", "reactive-var"], "client");
   api.use("underscore", "server");
+  api.use("iron:router@1.0.7");
 
   api.use("accounts-base", ["client", "server"]);
 
-  api.add_files(["adsso-client.js", "adsso.css", "adsso.html"], "client");
-  api.add_files("adsso-server.js", "server");
+  api.addFiles("adsso.html", "client");
+  api.addFiles("adsso.css", "client");
+  api.addFiles("adsso-client.js", "client");
+
+  api.addFiles("adsso-server.js", "server");
 
   api.export("SSO", ["client", "server"]);
 });
